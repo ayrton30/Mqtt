93d40b89beb1352501ecff16856b071a8cf5a5df
ayrton30 <42283019+ayrton30@users.noreply.github.com>
2018-08-22 09:21:25 -0300

    Add files via upload
    BrokerHandlerTest.java -> confirmar cuando se agrega/editar/elimina un broker.

--- MQTT/test/BrokerHandlerTest.java
+++ MQTT/test/BrokerHandlerTest.java
@@ -1,18 +1,50 @@
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class BrokerHandlerTest {
 
 	BrokerHandler manejadorB = new BrokerHandler();
 	
+	@Test
+	public void testAgregarBroker() {
+		int resultadoEsperado = manejadorB.getBrokers().size() + 1;
+		manejadorB.agregarBroker("nuevoHost", 1883);
+		
+		int resultadoReal = manejadorB.getBrokers().size();
+		assertEquals(resultadoEsperado, resultadoReal);
+	}
+	
+	@Test
+	public void testEditarBroker1() {
+		Broker resultadoEsperado = new Broker("editarHost", 1883);
+		manejadorB.agregarBroker("nuevoHost", 1883);
+		
+		manejadorB.editarBroker(0, resultadoEsperado.getHost(), resultadoEsperado.getPort());
+		Broker resultadoReal = manejadorB.getBroker(0);
+		
+		assertEquals(resultadoEsperado.getHost(), resultadoReal.getHost());
+		assertEquals(resultadoEsperado.getPort(), resultadoReal.getPort());
+	}
+	
 	@Test(expected=IndexOutOfBoundsException.class)
-	public void testEditarBroker() {
+	public void testEditarBroker2() {
 		//Tengo 2 brokers y quiero editar un broker inexistente (indice)
 		manejadorB.agregarBroker("primerHost", 1883);
 		manejadorB.editarBroker(-10, "nuevoHost", 9999);
 	}
 	
+	@Test
+	public void testEliminarBroker1() {
+		int resultadoEsperado = manejadorB.getBrokers().size() -1;
+		manejadorB.eliminarBroker(0);
+		
+		int resultadoReal = manejadorB.getBrokers().size();
+		assertEquals(resultadoEsperado, resultadoReal);
+	}
+	
 	@Test(expected=IndexOutOfBoundsException.class)
-	public void testEliminarBroker() {
+	public void testEliminarBroker2() {
 		manejadorB.eliminarBroker(9999);
 	}
 	
