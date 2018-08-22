b38ced7ee2ff9e5d02607b39df555002adc2be7c
ayrton30 <42283019+ayrton30@users.noreply.github.com>
2018-08-22 10:32:27 -0300

    Add files via upload

--- MQTT/src/PaqueteHandler.java
+++ MQTT/src/PaqueteHandler.java
@@ -27,7 +27,7 @@ public class PaqueteHandler implements Serializable {
 	}
 	
 	//Le cambio el topic a un paquete y le borro todos sus mensajes asociados!
-	public void editarPaquete(int i, String topic, boolean esSuscripcion) {
+	public void editarPaquete(int i, String topic, boolean esSuscripcion) throws IndexOutOfBoundsException {
 		if(esSuscripcion == true) {
 			this.paquetesS.get(i).setTopic(topic);
 			this.paquetesS.get(i).limpiarMensajes();
@@ -38,7 +38,7 @@ public class PaqueteHandler implements Serializable {
 		}
 	}
 		
-	public void eliminarPaquete(int i, boolean esSuscripcion) {
+	public void eliminarPaquete(int i, boolean esSuscripcion) throws IndexOutOfBoundsException {
 		if(esSuscripcion == true) {
 			this.paquetesS.remove(i);
 		}
@@ -47,7 +47,7 @@ public class PaqueteHandler implements Serializable {
 		}
 	}
 		
-	public Paquete getPaquete(int i, boolean esSuscripcion) {
+	public Paquete getPaquete(int i, boolean esSuscripcion) throws IndexOutOfBoundsException {
 		if(esSuscripcion == true) {
 			return(this.paquetesS.get(i));
 		}
